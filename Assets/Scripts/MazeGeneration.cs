e057d18aaf880466bd752c86c3f8417dc50c1f65
Sage <robertfernandez71@yahoo.com>
2016-04-28 22:16:30 -0700

    added position tracking for blocks

--- Assets/Scripts/MazeGeneration.cs
+++ Assets/Scripts/MazeGeneration.cs
@@ -13,12 +13,15 @@ public class MazeGeneration : MonoBehaviour {
     float tracery;
     float tracerz;
     GameObject[] MazeObjects;
+    int MazeObCount;
     Hub Hubref;
     Vector3[] PositionRegister;
-    const int maxObjects = 100;
+    const int maxObjects = 1000;
     int PositionCount;
     int activePosition;
     float[] positionmultiple = new float[4]{ .5f, -.5f, 1.5f, -1.5f };
+    string[] objectpaths = new string[4] { "Straight", "Branch", "End" , "Hub"};
+    int[] objectpotential = new int[4] { 75, 85, 95, 100 };
     private System.Random rnd;
 
     // Use this for initialization
@@ -27,16 +30,17 @@ public class MazeGeneration : MonoBehaviour {
         MazeObjects = new GameObject[maxObjects];
         PositionRegister = new Vector3[maxObjects];
         PositionCount = 0;
-        MazeObjects[0] = makeHub(0, 0, 0);
-        Hubref= MazeObjects[0].AddComponent<Hub>();
-        Debug.Log("Catch"+Hubref.returnContacts());
+        MazeObCount = 0;
+        MazeObCount++;
+        MazeObjects[MazeObCount] = makeHub(0, 0, 0);
+       // Hubref= MazeObjects[0].GetComponent<Hub>();
+
         activePosition = gencoords();
-        int contactTrace = 6;
+        int contactTrace = rnd.Next(2, 7);
 
         for (int i = 0; i < contactTrace; i++)
         {
             createpath(activePosition);
-            Debug.Log(activePosition);
             activePosition = gencoords();
         }
 
@@ -44,7 +48,6 @@ public class MazeGeneration : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
     }
 
     
@@ -56,17 +59,26 @@ public class MazeGeneration : MonoBehaviour {
 
     void makeStraight (float x, float y, float z)
     {
-        Instantiate(Resources.Load("Straight"), new Vector3(x, y, z), transform.rotation);
+        PositionRegister[PositionCount] = new Vector3(x, y, z);
+        MazeObjects[MazeObCount] = (GameObject)Instantiate(Resources.Load("Straight"), PositionRegister[PositionCount], transform.rotation);
+        PositionCount++;
+        MazeObCount++;
     }
 
     void makeBranch(float x, float y, float z)
     {
-        Instantiate(Resources.Load("Branch"), new Vector3(x, y, z), transform.rotation);
+        PositionRegister[PositionCount] = new Vector3(x, y, z);
+        MazeObjects[MazeObCount] = (GameObject)Instantiate(Resources.Load("Branch"), new Vector3(x, y, z), transform.rotation);
+        PositionCount++;
+        MazeObCount++;
     }
 
     void makeEnd(float x, float y, float z)
     {
-        Instantiate(Resources.Load("End"), new Vector3(x, y, z), transform.rotation);
+        PositionRegister[PositionCount] = new Vector3(x, y, z);
+        MazeObjects[MazeObCount] = (GameObject)Instantiate(Resources.Load("End"), new Vector3(x, y, z), transform.rotation);
+        MazeObCount++;
+        PositionCount++;
     }
 
     int gencoords()
@@ -93,12 +105,22 @@ public class MazeGeneration : MonoBehaviour {
 
     void createpath(int inc)
     {
+        int makeitem = 0;
         while (multx< LIMIT && multx > NLIMIT && multy < LIMIT && multy > NLIMIT && multz < LIMIT && multz > NLIMIT)
         {
-            makeStraight(multx, multy, multz);
+            makeitem = rnd.Next(0, 95);
+            if (makeitem<objectpotential[0]) makeStraight(multx, multy, multz);
+            else if (makeitem<objectpotential[1]) makeBranch(multx, multy, multz);
+            else if (makeitem<objectpotential[2])//if end
+            {
+                makeEnd(multx, multy, multz);
+                return;
+            }
+
             if (inc == 1) multx++;
             else if (inc == 2) multx--;
             else if (inc == 3) multy++;
+
             else if (inc == 4) multy--;
             else if (inc == 5) multz++;
             else multz--; //inc==5
