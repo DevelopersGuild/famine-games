87f74e0071c52a9ed1816cbeeed8cfccb9cbf0fc
dbirtola <dbirtola@gmail.com>
2016-04-29 11:48:51 -0700

    Discobot testing

---
+++ Assets/Capsule.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Capsule : MonoBehaviour {
+    
+    public GameObject weapon;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+
+    void equip()
+    {
+        GameObject temp = Instantiate(weapon, Vector3.zero, Quaternion.identity) as GameObject;
+        temp.transform.parent = gameObject.transform;
+    }
+
+}
