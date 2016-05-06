fb2ddff1fbe347fd0c10adef15aaacb6a532cdf6
Edward Yi <Edward Yi>
2016-05-06 14:19:46 -0700

    Worked on Traps

---
+++ Assets/Scripts/ItemsAndObjects/Damage Trap.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class DamageTrap : MonoBehaviour {
+
+    public int timer;
+    private bool isactivated;
+    private int currentHealth;
+    private Health player;
+    public int healthDeBuffAmount = 5;
+
+    // Use this for initialization
+    void Start () {
+        isactivated = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        player = other.GetComponent<Health>();
+        if (player != null)
+        {
+            currentHealth = player.currentHealth;
+            player.currentHealth -= healthDeBuffAmount;
+            StartCoroutine("playerHealthDeBuffTimer");
+        }
+    }
+
+    IEnumerator playerHealthDeBuffTimer()
+    {
+        yield return new WaitForSeconds(timer);
+        playerHealthDeBuffUndo();
+    }
+
+    void playerHealthDeBuffUndo()
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/ItemsAndObjects/Trap.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Trap : MonoBehaviour {
+
+    public int timer;
+    private bool isactivated;
+    private float originalSpeed;
+    private FirstPersonController player;
+
+	// Use this for initialization
+	void Start () {
+        isactivated = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        player = other.GetComponent<FirstPersonController>();
+        if (player != null)
+        {
+            originalSpeed = player.m_WalkSpeed;
+            player.m_WalkSpeed = 2;
+            StartCoroutine("playerSpeedDebuffTimer");
+        }
+    }
+
+    IEnumerator playerSpeedDebuffTimer()
+    {
+        yield return new WaitForSeconds(timer);
+        playerSpeedDebuffUndo();
+    }
+
+    void playerSpeedDebuffUndo()
+    {
+        player.m_WalkSpeed = originalSpeed;
+    }
+
+
+}
