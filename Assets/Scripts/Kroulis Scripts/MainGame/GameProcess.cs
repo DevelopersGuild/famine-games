96b30def9b651caaa7997188fe2f466f05b16698
kroulis <qinkroulis@gmail.com>
2016-05-28 15:13:18 -0700

    Start Game Countdown 2

--- Assets/Scripts/Kroulis Scripts/MainGame/GameProcess.cs
+++ Assets/Scripts/Kroulis Scripts/MainGame/GameProcess.cs
@@ -183,6 +183,7 @@ namespace Kroulis.Components
                 return;
             if(times<0)
                 return;
+            RpcShowStartGameTips("Game will start in " + times.ToString("F1") + "s...");
         }
 
         [ClientRpc]
