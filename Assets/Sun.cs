07bde51cf3988f944202180054eb59a8bd51fef7
dbirtola <dbirtola@gmail.com>
2016-06-10 15:22:11 -0700

    Polished arena level

---
+++ Assets/Sun.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sun : MonoBehaviour {
+
+    public float rotationSpeed = 2;
+
+    Transform centerOfEarth;
+
+	// Use this for initialization
+	void Start () {
+
+        centerOfEarth = gameObject.transform.parent;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.Rotate(centerOfEarth.position, rotationSpeed * Time.deltaTime);
+	}
+}
