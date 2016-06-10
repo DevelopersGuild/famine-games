66dc90ab5cca99ad69b2a865832bb5bc27463b4d
Edward Yi <Edward Yi>
2016-06-10 13:56:33 -0700

    Create control points

---
+++ Assets/Prefabs/EnvironmentsP/controlPoint/controlpointContainer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class controlpointContainer : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    foreach(Transform children in transform) {
+        arrayList.add(children)
+    }
+
+    controlPoint = child.GetComponent<ControlPoint>();
+}
